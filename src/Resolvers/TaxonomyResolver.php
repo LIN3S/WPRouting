663237c30ddcb2dbc9b5a76d0e9fbdb0c1df74c5
benatespina <benatespina@gmail.com>
2015-06-18 18:26:08 +0200

    Fixed a bug about taxonomy resolver

--- src/Resolvers/TaxonomyResolver.php
+++ src/Resolvers/TaxonomyResolver.php
@@ -4,17 +4,24 @@ namespace LIN3S\WPRouting\Resolvers;
 
 use LIN3S\WPRouting\RouteRegistry;
 
-class TaxonomyResolver extends Resolver {
-
+class TaxonomyResolver extends Resolver
+{
     protected $types = ['taxonomy', 'archive'];
 
     public function resolve(RouteRegistry $routes)
     {
         $term = get_queried_object();
 
-        if ( ! empty( $term->slug ) ) {
+        if (!empty($term->slug)) {
             $controllers = $routes->getByTypeAndSlug('taxonomy', $term->slug);
-            if(count($controllers) > 0) {
+            if (count($controllers) > 0) {
+                return $controllers[0];
+            }
+        }
+
+        if (!empty($term->taxonomy)) {
+            $controllers = $routes->getByTypeAndSlug('taxonomy', $term->taxonomy);
+            if (count($controllers) > 0) {
                 return $controllers[0];
             }
         }
